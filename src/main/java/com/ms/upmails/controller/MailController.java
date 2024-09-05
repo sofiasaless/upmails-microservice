96423db8ebe128db59b453dfa21bb292e09a4c3f
sofiasales02 <sofiasaleswk@gmail.com>
2024-09-05 17:31:14 -0300

    feat: crossorigin on controller

--- src/main/java/com/ms/upmails/controller/MailController.java
+++ src/main/java/com/ms/upmails/controller/MailController.java
@@ -3,6 +3,7 @@ package com.ms.upmails.controller;
 import org.springframework.beans.BeanUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,6 +18,7 @@ import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class MailController {
     private final MailService mailService;
 
