d94b81c65df818d4a817301ec3f8f4889f761720
sofiasales02 <sofiasaleswk@gmail.com>
2024-09-05 16:02:26 -0300

    initial commit

---
+++ src/main/java/com/ms/upmails/UpmailsApplication.java
@@ -0,0 +1,13 @@
+package com.ms.upmails;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpmailsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpmailsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ms/upmails/UpmailsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ms.upmails;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpmailsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
